7516f5f0c433e460524fa7e6c69971fbed79d480
Hunter3ds <ghostmat817@gmail.com>
2025-03-26 16:58:08 -0300

    :lipstick: feat: adicionando o componente banner

---
+++ src/components/Banner.js
@@ -0,0 +1,32 @@
+'use client';
+
+import {useState} from 'react';
+import Image from 'next/image'
+import styles from './Banner.module.css';
+
+export default function Banner(){
+    const [showPromo, setShowPromo] = useState(false)   ;
+    return(
+        <div className={styles.banner}>
+            <Image
+            src='/burguer.png'
+            alt='Delicioso hambúrguer artesanal'
+            fill
+            quality={100}
+            style={{objectFit: 'cover'}}
+            />
+            <div className={styles.overlay}>
+                <h1 className={styles.title}>O mlehor Hambúrguer da Cidade!</h1>
+                <p className={styles.text}>Sabor inigualável, ingredientes frescos e uma experiência única para seu paladar.</p>
+                <button className={styles.button} onClick={()=> setShowPromo(true)}>Ver promoção</button>
+            </div>
+            {/* Modal de Promoção */}
+            <div className={`${styles.modal} ${showPromo? styles.show: ''}`}>
+                <div className={styles.modalContent}>
+                    <button className={styles.close} onClick={()=> setShowPromo(false)}>&times;</button>
+                    <p className={styles.promo}>🎉 Promoção: Combo Especial por apenas R$ 29,90! 🎉</p>
+                </div>
+            </div>
+        </div>
+    );
+};
