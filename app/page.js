1b91db9eb41081aad843150bbbdd087b82e83745
Hunter3ds <ghostmat817@gmail.com>
2025-04-09 15:45:51 -0300

    :lipstick: feat: containers de cardapio

--- app/page.js
+++ app/page.js
@@ -5,7 +5,9 @@ import Menu from '../src/components/Menu'
 
 export default function Home() {
   const menuItems=[
-    { id: 1, src: '/cheese-bacon.png'   alt:}
+    {id:1, name: 'Cheese Bacon', price: 'R$ 25,90', src: '/cheese-bacon.png', alt: 'Hamburguer de queijo com bacon' },
+    {id:2, name: 'Veggie', price:'R$ 30,50', src:'/veggie-burger.png', alt: 'Haburguer vegetariano'},
+    {id:3, name:'SmashX', price:'R$ 27,85', src:'/smash-burguer.webp', alt:'O completão da casa'}
   ] 
 
   return (
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,18 +1,24 @@
 'use client';
 import Image from "next/image";
+import styles from './Menu.module.css'
 
 export default function Menu({items}){
     return(
-        <div>
-            {items.map((item, index)=>(
-                <div key={item.id || index}>
-                    <Image 
-                    src={item.src} 
-                    alt={item.alt} 
-                    width={500} 
-                    height={500}/>
-                </div>
-            ))}
+        <div className={styles.menu}>
+            <h2>Cardapio de lanches:</h2>
+            <div className={styles.cardContainer}>
+                {items.map((item, index)=>(
+                    <div className={styles.card} key={item.id || index}>
+                        <Image 
+                        src={item.src} 
+                        alt={item.alt} 
+                        width={150} 
+                        height={150}/>
+                        <h3>{item.name}</h3>
+                        <p>{item.price}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
